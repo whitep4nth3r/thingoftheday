7e9b2ff95490cd67fe95dd817f25affac847f494
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2021-01-18 20:25:57 +0000

    :sparkles: Fetch data from GraphQL API and render in the DOM

---
+++ TUTORIAL.md
@@ -0,0 +1,30 @@
+# Building a lightweight microblogging site with Contentful and ExpressJS
+
+## Prepare your development environment
+
+## Define your content model
+
+The naming of the content model is important because you can't edit this easily!
+(I called it text first, and it made no sense...)
+
+- text - optional
+- image - optional
+- panther emote (list) - required
+- date (auto)
+- link - optional
+
+- embeds???
+
+## Add some content
+
+...
+
+## Fetch the content
+
+... see app.js
+
+mention the access token is 'ok' to be exposed as it's read only on the CDA
+
+## Style the content
+
+...
--- app.js
+++ app.js
@@ -1,1 +1,70 @@
-console.log('thingoftheday is working')
+const spaceID = "4nhaj6wzvnco";
+const endpoint = "https://graphql.contentful.com/content/v1/spaces/" + spaceID;
+const accessToken = "F91A7b3FyjTFeH0sN6pYIfo6Nu1WZ2byX8Rdc4McGUI";
+const query = `query{
+  textCollection {
+    items {
+      text
+      image {
+        url
+        title
+      }
+      panther
+      link
+    }
+  }
+}`
+
+const fetchOptions = {
+  spaceID,
+  accessToken,
+  endpoint,
+  method: 'POST',
+  headers: {
+    Authorization: "Bearer " + accessToken,
+    "Content-Type": "application/json",
+  },
+  redirect: 'follow',
+  referrerPolicy: 'no-referrer',
+  body: JSON.stringify({ query })
+}
+
+const articleHolder = document.querySelector('[data-items]');
+
+const populateDom = (items) => {
+  items.forEach(item => {
+    const newItemEl = document.createElement('article');
+    let newTextEl, newImgEl, newLinkEl;
+
+    const newPantherEl = document.createElement('img');
+    newPantherEl.src = `./panthers/${item.panther}.png`;
+    newPantherEl.alt = `Image of ${item.panther} panther emote`;
+    newItemEl.appendChild(newPantherEl);
+
+    if (item.text) {
+      newTextEl = document.createElement('h2');
+      newTextEl.innerText = item.text;
+      newItemEl.appendChild(newTextEl);
+    }
+
+    if (item.image) {
+      newImgEl = document.createElement('img');
+      newImgEl.src = item.image.url;
+      newImgEl.alt = item.image.title;
+      newItemEl.appendChild(newImgEl);
+    }
+
+    if (item.link) {
+      newLinkEl = document.createElement('a');
+      newLinkEl.href = item.link;
+      newItemEl.appendChild(newLinkEl);
+    }
+
+    articleHolder.appendChild(newItemEl);
+  })
+}
+
+fetch(endpoint, fetchOptions)
+  .then(response => response.json())
+  .then(data => populateDom(data.data.textCollection.items));
+
