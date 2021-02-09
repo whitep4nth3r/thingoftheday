cdf393a028a458f387b5b4aa14fc77954fea3857
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2021-02-09 17:21:35 +0000

    :alembic: Listen to Stefan and convert xml nonsense to string

--- functions/rss.js
+++ functions/rss.js
@@ -1,32 +1,16 @@
 exports.handler = async function (event, context) {
-  const doc = document.implementation.createDocument("", "", null);
-
-  const rssElem = doc.createElement("rss");
-  rssElem.setAttribute("version", "2.0");
-
-  const channelElem = doc.createElement("channel");
-
-  const titleElem = doc.createElement("title");
-  titleElem.innerHTML = "thingoftheday.xyz";
-  channelElem.appendChild(titleElem);
-
-  const linkElem = doc.createElement("link");
-  linkElem.innerHTML = "https://thingoftheday.xyz";
-  channelElem.appendChild(linkElem);
-
-  const descriptionElem = doc.createElement("description");
-  descriptionElem.innerHTML =
-    "thingoftheday is a lightweight microblogging site powered by Contentful and vanilla HTML, CSS and JavaScript.";
-  channelElem.appendChild(descriptionElem);
-
-  rssElem.appendChild(channelElem);
-  doc.appendChild(rssElem);
-
-  console.log(doc);
+  const rssFeed = `<?xml version="1.0"?>
+  <rss version="2.0">
+  <channel>
+  <title>thingoftheday.xyz</title>
+  <link>https://thingoftheday.xyz</link>
+  <description>thingoftheday is a lightweight microblogging site powered by Contentful and vanilla HTML, CSS and JavaScript.</description>
+  </channel>
+  </rss>`;
 
   return {
     statusCode: 200,
     contenType: "text/xml",
-    body: doc,
+    body: rssFeed,
   };
 };
