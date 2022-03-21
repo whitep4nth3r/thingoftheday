9c7c10cd364e50c50dd7481398205ea14dc286b1
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2022-03-21 14:00:44 +0000

    Fix valid dates for RSS feed

--- functions/rss.js
+++ functions/rss.js
@@ -1,5 +1,32 @@
 const https = require("https");
 
+function addLeadingZero(num) {
+  num = num.toString();
+  while (num.length < 2) num = "0" + num;
+  return num;
+}
+
+// See https://github.com/whitep4nth3r/rfc-822
+// and https://whitep4nth3r.com/blog/how-to-format-dates-for-rss-feeds-rfc-822/
+
+function buildRFC822Date(dateString) {
+  const dayStrings = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const monthStrings = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+  const timeStamp = Date.parse(dateString);
+  const date = new Date(timeStamp);
+
+  const day = dayStrings[date.getDay()];
+  const dayNumber = addLeadingZero(date.getDate());
+  const month = monthStrings[date.getMonth()];
+  const year = date.getFullYear();
+  const time = `${addLeadingZero(date.getHours())}:${addLeadingZero(date.getMinutes())}:00`;
+  const timezone = date.getTimezoneOffset() === 0 ? "GMT" : "BST";
+
+  //Wed, 02 Oct 2002 13:00:00 GMT
+  return `${day}, ${dayNumber} ${month} ${year} ${time} ${timezone}`;
+}
+
 async function getPosts() {
   return new Promise((resolve, reject) => {
     const query = `
@@ -72,7 +99,7 @@ function buildRssItems(items) {
         <author>whitep4nth3r@gmail.com (whitep4nth3r)</author>
         <link>https://thingoftheday.xyz#${item.sys.id}</link>
         <guid>https://thingoftheday.xyz#${item.sys.id}</guid>
-        <pubDate>${item.sys.firstPublishedAt}</pubDate>
+        <pubDate>${buildRFC822Date(item.sys.firstPublishedAt)}</pubDate>
         </item>
         `;
     })
